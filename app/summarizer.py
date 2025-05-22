a602db5c71e8f61967e9336763e27a03493e1bee
unknown <shringarkashyap7@gmail.com>
2025-05-23 02:28:44 +0530

    adding summarizer logic

---
+++ app/summarizer.py
@@ -0,0 +1,10 @@
+from transformers import pipeline
+
+class Summarizer:
+    def __init__(self):
+        self.pipeline = pipeline("summarization", model="sshleifer/distilbart-cnn-12-6")
+
+    def summarize(self, text: str) -> str:
+        result = self.pipeline(text, max_length=130, min_length=30, do_sample=False)
+        return result[0]["summary_text"]
+

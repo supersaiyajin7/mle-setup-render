b6bcdc4dca637174a400aca0176387c89ce5bdf9
unknown <shringarkashyap7@gmail.com>
2025-05-25 01:10:12 +0530

    changing test cases to adjust labels

--- tests/test_model.py
+++ tests/test_model.py
@@ -5,7 +5,9 @@ model = SentimentModel()
 
 def test_model_positive():
     result = model.predict("I love this!")
-    assert result["label"] in ["POSITIVE", "NEGATIVE"]
+    assert result["label"] in [
+            "1 star", "2 stars", "3 stars", "4 stars", "5 stars"
+        ]
     assert 0 <= float(result["score"]) <= 1
 
 def test_model_empty_input():
